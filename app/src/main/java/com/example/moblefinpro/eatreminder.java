e71af0b5a25a727ac1d9cb25886eb853866a70ec
Ethan Elnathan Mathias <91774181+ethanelnathan@users.noreply.github.com>
2023-05-12 19:05:28 +0700

    First Front End Progress
    pages 2, 5, 6, 9
    pages left: 1, 3, 4, 7, logo page

---
+++ app/src/main/java/com/example/moblefinpro/eatreminder.java
@@ -0,0 +1,14 @@
+package com.example.moblefinpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eatreminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eatreminder);
+    }
+}
