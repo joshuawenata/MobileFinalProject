8b8ca2c738e8b2dee31397f8f53d7a69452ab1a1
Ethan Elnathan Mathias <91774181+ethanelnathan@users.noreply.github.com>
2023-05-13 16:47:30 +0700

    pushemptyfrontendfiles
    pushemptyfrontendfiles

---
+++ app/src/main/java/com/example/moblefinpro/SleepReminder.java
@@ -0,0 +1,14 @@
+package com.example.moblefinpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_reminder);
+    }
+}
