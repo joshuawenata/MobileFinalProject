c8c8fdbd1e771b57d6f36a78c0cf7549323939a7
Ethan Elnathan Mathias <91774181+ethanelnathan@users.noreply.github.com>
2023-05-14 01:56:35 +0700

    recycler view and adapters for excersize page
    recycler view and adapters for excersize page

--- app/src/main/java/com/example/moblefinpro/ExcersizeReminder.java
+++ app/src/main/java/com/example/moblefinpro/ExcersizeReminder.java
@@ -1,8 +1,12 @@
 package com.example.moblefinpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExcersizeReminder extends AppCompatActivity {
 
@@ -11,4 +15,12 @@ public class ExcersizeReminder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excersize_reminder);
     }
+    RecyclerView recyclerView = findViewById(R.id.recyclerView_ex);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    List<String> data = new ArrayList<>();
+    data.add("Item 1");
+    data.add("Item 2");
+    data.add("Item 3");
+    recyclerAdapter adapter = new recyclerAdapter(data);
+    recyclerView.setAdapter(adapter);
 }
