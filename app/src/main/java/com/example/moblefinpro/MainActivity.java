1e78f152bd71e4c3642896813ccbd7937811037e
Ethan Elnathan Mathias <91774181+ethanelnathan@users.noreply.github.com>
2023-05-13 15:40:57 +0700

    page 1 front end done
    improved page 1 with scroll view

--- app/src/main/java/com/example/moblefinpro/MainActivity.java
+++ app/src/main/java/com/example/moblefinpro/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.moblefinpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
