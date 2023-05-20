104f371eabf3618bb0f976131e5047812e8cc2b5
Ethan Elnathan Mathias <91774181+ethanelnathan@users.noreply.github.com>
2023-05-21 02:30:18 +0700

    sleepreminderUI and Water reminder progress
    sleepreminderUI and Water reminder progress

--- app/src/main/java/com/example/moblefinpro/SleepReminder.java
+++ app/src/main/java/com/example/moblefinpro/SleepReminder.java
@@ -3,6 +3,7 @@ package com.example.moblefinpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class SleepReminder extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class SleepReminder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_reminder);
     }
+    TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
+    picker.setIs24HourView(true);
 }
--- app/src/main/java/com/example/moblefinpro/WaterReminder.java
+++ app/src/main/java/com/example/moblefinpro/WaterReminder.java
@@ -2,13 +2,34 @@ package com.example.moblefinpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class WaterReminder extends AppCompatActivity {
+
+    AnimationDrawable waveAnimated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_reminder);
+//        WaveLoadingView mWaveLoadingView = (WaveLoadingView) findViewById(R.id.waveLoadingView);
+//        mWaveLoadingView.setShapeType(WaveLoadingView.ShapeType.CIRCLE);
+//        mWaveLoadingView.setTopTitle("Top Title");
+//        mWaveLoadingView.setCenterTitleColor(Color.GRAY);
+//        mWaveLoadingView.setBottomTitleSize(18);
+//        mWaveLoadingView.setProgressValue(80);
+//        mWaveLoadingView.setBorderWidth(10);
+//        mWaveLoadingView.setAmplitudeRatio(60);
+//        mWaveLoadingView.setWaveColor(Color.GRAY);
+//        mWaveLoadingView.setBorderColor(Color.GRAY);
+//        mWaveLoadingView.setTopTitleStrokeColor(Color.BLUE);
+//        mWaveLoadingView.setTopTitleStrokeWidth(3);
+//        mWaveLoadingView.setAnimDuration(3000);
+//        mWaveLoadingView.pauseAnimation();
+//        mWaveLoadingView.resumeAnimation();
+//        mWaveLoadingView.cancelAnimation();
+//        mWaveLoadingView.startAnimation();
     }
 }
