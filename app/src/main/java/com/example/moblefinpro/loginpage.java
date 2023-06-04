939409a92d4af29f647679e58fc9bd603c311c50
joshuawenata <joshuawenata@gmail.com>
2023-06-04 16:08:08 +0700

    change app name + change mipmap + google sign in

--- app/src/main/java/com/example/moblefinpro/loginpage.java
+++ app/src/main/java/com/example/moblefinpro/loginpage.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 public class loginpage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
     }
 }
