13815c15ed5b91b689f796fd6565d92e4e7d4eab
Jelshen Febryand Valesco <jelshenfebryandvalesco@gmail.com>
2023-06-16 12:49:12 +0700

    yes

--- app/src/main/java/com/example/moblefinpro/AddExercise.java
+++ app/src/main/java/com/example/moblefinpro/AddExercise.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AddExcersize extends AppCompatActivity {
+public class AddExercise extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/moblefinpro/exerciseLayout.java
+++ app/src/main/java/com/example/moblefinpro/exerciseLayout.java
@@ -1,19 +1,17 @@
 package com.example.moblefinpro;
 
-import android.media.Image;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class excersizeLayout extends RecyclerView.ViewHolder {
+public class exerciseLayout extends RecyclerView.ViewHolder {
     TextView nameView,repstitles,hourtitle;
     Switch exswitch;
 
-    public excersizeLayout(@NonNull View itemView) {
+    public exerciseLayout(@NonNull View itemView) {
         super(itemView);
         nameView = itemView.findViewById(R.id.exercizelayout_restitle);
         repstitles = itemView.findViewById(R.id.exercizelayout_repstext);
