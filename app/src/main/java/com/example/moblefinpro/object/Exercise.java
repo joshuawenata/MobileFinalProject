57a182e8d7604b0b702c5208d0ecc5c4f162dfc1
joshuawenata <joshuawenata@gmail.com>
2023-06-24 22:32:31 +0700

    hello, name

---
+++ app/src/main/java/com/example/moblefinpro/object/Exercise.java
@@ -0,0 +1,29 @@
+package com.example.moblefinpro.object;
+
+public class Exercise {
+    private String name, repetition, time;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRepetition() {
+        return repetition;
+    }
+
+    public void setRepetition(String repetition) {
+        this.repetition = repetition;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
